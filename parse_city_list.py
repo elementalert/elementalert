3aa312b14a8cec4cd52b72207174bbe32ff0e0ba
elementalert <elementalert@gmail.com>
2015-12-09 00:06:11 -0500

    parse_city_list  source:  geonames.org

--- parse_city_list.py
+++ parse_city_list.py
@@ -8,33 +8,64 @@
 
 from pymongo import MongoClient
 from datetime import datetime
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--updatecodes', dest='UPDATE_CODES', action='store_true')
+args = parser.parse_args()
+
 
 client = MongoClient()
 
 db = client['country_data']
 
-with open('country_codes.txt') as country_codes_file:
-    # AF,"Afghanistan"
 
-    for line in country_codes_file:
-        print line
-        (code, name) = line.rstrip().split(',', 1)
+if args.UPDATE_CODES:
+    with open('country_codes.txt') as country_codes_file:
+        # AF,"Afghanistan"
 
-        db['codes'].insert({
-            'code': code.upper(),
-            'name': name.strip("\""),
-            'date': datetime.utcnow()
-        })
+        for line in country_codes_file:
+            print line
+            (code, name) = line.rstrip().split(',', 1)
 
-with open('cities.txt') as cities_file:
-    # Country,City,AccentCity,Region,Population,Latitude,Longitude  (maxmind.com)
+            db['codes'].insert({
+                'code': code.upper(),
+                'name': name.strip("\""),
+                'date': datetime.utcnow()
+            })
 
-    cities_file.next()    # Chomp the above header line
+with open('cities2.txt') as cities_file:
+    #     Source: GeoNames.org
+    #     The main 'geoname' table has the following fields :
+    # ---------------------------------------------------
+    # geonameid         : integer id of record in geonames database
+    # name              : name of geographical point (utf8) varchar(200)
+    # asciiname         : name of geographical point in plain ascii characters, varchar(200)
+    # alternatenames    : alternatenames, comma separated, ascii names automatically transliterated, convenience attribute from alternatename table, varchar(10000)
+    # latitude          : latitude in decimal degrees (wgs84)
+    # longitude         : longitude in decimal degrees (wgs84)
+    # feature class     : see http://www.geonames.org/export/codes.html, char(1)
+    # feature code      : see http://www.geonames.org/export/codes.html, varchar(10)
+    # country code      : ISO-3166 2-letter country code, 2 characters
+    # cc2               : alternate country codes, comma separated, ISO-3166 2-letter country code, 200 characters
+    # admin1 code       : fipscode (subject to change to iso code), see exceptions below, see file admin1Codes.txt for display names of this code; varchar(20)
+    # admin2 code       : code for the second administrative division, a county in the US, see file admin2Codes.txt; varchar(80)
+    # admin3 code       : code for third level administrative division, varchar(20)
+    # admin4 code       : code for fourth level administrative division, varchar(20)
+    # population        : bigint (8 byte int)
+    # elevation         : in meters, integer
+    # dem               : digital elevation model, srtm3 or gtopo30, average elevation of 3''x3'' (ca 90mx90m) or 30''x30'' (ca 900mx900m) area in meters, integer. srtm processed by cgiar/ciat.
+    # timezone          : the timezone id (see file timeZone.txt) varchar(40)
+    # modification date : date of last modification in yyyy-MM-dd format
 
     for line in cities_file:
-        (code, city, accent_city, region, population, latitude, longitude) = line.split(',')
-        code_doc = db['codes'].find_one({'code': code.upper()})
 
+        (_geonameid, city_name, ascii_name, _alternatenames, _lat, _long, _feat_class, _feat_code, country_code,
+            cc2, _admin1, _admin2, _admin3, _admin4, _pop, _elev, _dem, _timezone, _mod_date) = line.rstrip().split('\t')
+
+        print "%s\t%s\t%s" % (name, ascii_name, country_code)
+
+        code_doc = db['codes'].find_one({'code': code.upper()})
         country = code_doc['name']
 
         db['cities'].insert({
@@ -42,5 +73,11 @@ with open('cities.txt') as cities_file:
             'country': country,
             'date': datetime.utcnow()
         })
-        print "Inserting: Country - %s (%s)\t\tCity - %s" % (country, code.upper(), city)
-        db['countries'].update({'name': country}, {'$push': {'cities': city}})
+
+        print "Inserting: Country - %s (%s)\t\tCity - %s" % (country, code.upper(), city_name)
+        db['countries'].update({'name': country}, {'$push': {'cities': city_name}})
+
+
+
+
+
